3ddd2b730fc502a06cacaa68140e05116ccc33fb
Doug Gregor <dgregor@apple.com>
2024-10-11 12:58:44 -0700

    License fix

--- Sources/JavaKitVM/LockedState.swift
+++ Sources/JavaKitVM/LockedState.swift
@@ -2,11 +2,13 @@
 //
 // This source file is part of the Swift.org open source project
 //
-// Copyright (c) 2022 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
+// Copyright (c) 2024 Apple Inc. and the Swift.org project authors
+// Licensed under Apache License v2.0
 //
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
 
