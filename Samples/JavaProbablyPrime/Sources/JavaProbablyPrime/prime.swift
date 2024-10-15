ddc9ca94c75516819121331097166ac5fca0baab
Doug Gregor <dgregor@apple.com>
2024-10-14 22:46:46 -0700

    Add a JavaKit sample that produces a Swift command-line app using Java APIs
    This demonstrates how to pull in just one Java type (java.math.BigInteger)
    and use it from a Swift program (a command line app).

---
+++ Samples/JavaProbablyPrime/Sources/JavaProbablyPrime/prime.swift
@@ -0,0 +1,38 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2024 Apple Inc. and the Swift.org project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import ArgumentParser
+import JavaKit
+import JavaKitVM
+
+@main
+struct ProbablyPrime: ParsableCommand {
+  @Argument(help: "The number to check for primality")
+  var number: String
+
+  @Option(help: "The certainty to require in the prime check")
+  var certainty: Int32 = 10
+
+  func run() throws {
+    let javaVirtualMachine = try JavaVirtualMachine.shared()
+    let jniEnvironment = try javaVirtualMachine.environment()
+
+    let bigInt = BigInteger(number, environment: jniEnvironment)
+    if bigInt.isProbablePrime(certainty) {
+      print("\(number) is probably prime")
+    } else {
+      print("\(number) is definitely not prime")
+    }
+  }
+}
