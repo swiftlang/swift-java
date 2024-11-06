5e47f351539a7a02f7851ce6c74a66e3538a74eb
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2024-11-06 22:17:57 +0900

    jextract fix variable test

--- Tests/JExtractSwiftTests/VariableImportTests.swift
+++ Tests/JExtractSwiftTests/VariableImportTests.swift
@@ -34,138 +34,164 @@ final class VariableImportTests {
     """
 
   @Test("Import: var counter: Int")
-  func variable_() throws {
+  func variable_int() throws {
     let st = Swift2JavaTranslator(
       javaPackage: "com.example.swift",
-      swiftModuleName: "__FakeModule"
+      swiftModuleName: "FakeModule"
     )
     st.log.logLevel = .error
 
-    try st.analyze(file: "Fake.swift", text: class_interfaceFile)
-
-    let identifier = "counterInt"
-    let varDecl: ImportedVariable? =
-      st.importedTypes.values.compactMap {
-          $0.variables.first {
-            $0.identifier == identifier
-          }
-        }.first
-    guard let varDecl else {
-      fatalError("Cannot find: \(identifier)")
-    }
-
-    let output = CodePrinter.toString { printer in
-      st.printVariableDowncallMethods(&printer, varDecl)
-    }
-
-    assertOutput(
-      dump: true,
-      output,
-      expected:
-      """
-      // ==== --------------------------------------------------
-      // counterInt
-      private static class counterInt {
-        public static final FunctionDescriptor DESC_GET =   FunctionDescriptor.of(
-              /* -> */SWIFT_INT,
-            SWIFT_POINTER
-          );
-        public static final MemorySegment ADDR_GET = __FakeModule.findOrThrow("g");
-        public static final MethodHandle HANDLE_GET = Linker.nativeLinker().downcallHandle(ADDR_GET, DESC_GET);
-        public static final FunctionDescriptor DESC_SET =   FunctionDescriptor.ofVoid(
-          SWIFT_INT,
-            SWIFT_POINTER
-          );
-        public static final MemorySegment ADDR_SET = __FakeModule.findOrThrow("s");
-        public static final MethodHandle HANDLE_SET = Linker.nativeLinker().downcallHandle(ADDR_SET, DESC_SET);
-      }
-      /**
-       * Function descriptor for:
-       *
-       */
-      public static FunctionDescriptor counterInt$get$descriptor() {
-          return counterInt.DESC_GET;
-      }
-      /**
-       * Downcall method handle for:
-       *
-       */
-      public static MethodHandle counterInt$get$handle() {
-          return counterInt.HANDLE_GET;
-      }
-      /**
-       * Address for:
-       *
-       */
-      public static MemorySegment counterInt$get$address() {
-          return counterInt.ADDR_GET;
-      }
-      /**
-       * Function descriptor for:
-       *
-       */
-      public static FunctionDescriptor counterInt$set$descriptor() {
-          return counterInt.DESC_SET;
-      }
-      /**
-       * Downcall method handle for:
-       *
-       */
-      public static MethodHandle counterInt$set$handle() {
-          return counterInt.HANDLE_SET;
-      }
-      /**
-       * Address for:
-       *
-       */
-      public static MemorySegment counterInt$set$address() {
-          return counterInt.ADDR_SET;
-      }
-      /**
-       * Downcall to Swift:
-       * 
-       */
-      public static long getCounterInt(java.lang.foreign.MemorySegment self$) {
-          var mh$ = counterInt.HANDLE_GET;
-          try {
-              if (TRACE_DOWNCALLS) {
-                  traceDowncall(self$);
-              }
-              return (long) mh$.invokeExact(self$);
-          } catch (Throwable ex$) {
-              throw new AssertionError("should not reach here", ex$);
-          }
-      }
-      /**
-       * Downcall to Swift:
-       *
-       */
-      public long getCounterInt() {
-        return (long) getCounterInt($memorySegment());
-      }
-      /**
-       * Downcall to Swift:
-       *
-       */
-      public static void setCounterInt(long newValue, java.lang.foreign.MemorySegment self$) {
-        var mh$ = counterInt.HANDLE_SET;
-        try {
-          if (TRACE_DOWNCALLS) {
-             traceDowncall(newValue, self$);
-          }
-           mh$.invokeExact(newValue, self$);
-        } catch (Throwable ex$) {
-          throw new AssertionError("should not reach here", ex$);
+    try assertOutput(
+      st, input: class_interfaceFile, .java,
+      detectChunkByInitialLines: 7,
+      expectedChunks:
+      [
+        """
+        private static class counterInt {
+          public static final FunctionDescriptor DESC_GET =     FunctionDescriptor.of(
+                /* -> */SWIFT_INT,
+              SWIFT_POINTER
+            );
+          public static final MemorySegment ADDR_GET = 
+            FakeModule.findOrThrow("swiftjava_FakeModule_MySwiftClass_counterInt$1");
+          public static final MethodHandle HANDLE_GET = Linker.nativeLinker().downcallHandle(ADDR_GET, DESC_GET);
+          public static final FunctionDescriptor DESC_SET =     FunctionDescriptor.ofVoid(
+            SWIFT_INT,
+              SWIFT_POINTER
+            );
+          public static final MemorySegment ADDR_SET =
+            FakeModule.findOrThrow("swiftjava_FakeModule_MySwiftClass_counterInt__$1");
+          public static final MethodHandle HANDLE_SET = Linker.nativeLinker().downcallHandle(ADDR_SET, DESC_SET);
         }
-      }
-      /**
-       * Downcall to Swift:
-       *
-       */
-      public void setCounterInt(long newValue) {
-         setCounterInt(newValue, $memorySegment());
-      }
-      """
+        """,
+        """
+        /**
+         * Function descriptor for:
+         * {@snippet lang=swift :
+         * public var counterInt: Int
+         * }
+         */
+        public static FunctionDescriptor counterInt$get$descriptor() {
+            return counterInt.DESC_GET;
+        }
+        """,
+        """
+        /**
+         * Downcall method handle for:
+         * {@snippet lang=swift :
+         * public var counterInt: Int
+         * }
+         */
+        public static MethodHandle counterInt$get$handle() {
+            return counterInt.HANDLE_GET;
+        }
+        """,
+        """
+        /**
+         * Address for:
+         * {@snippet lang=swift :
+         * public var counterInt: Int
+         * }
+        */
+        public static MemorySegment counterInt$get$address() {
+            return counterInt.ADDR_GET;
+        }
+        """,
+        """
+        /**
+         * Function descriptor for:
+         * {@snippet lang=swift :
+         * public var counterInt: Int
+         * }
+         */
+        public static FunctionDescriptor counterInt$set$descriptor() {
+            return counterInt.DESC_SET;
+        }
+        """
+        ,
+        """
+        /**
+         * Downcall method handle for:
+         * {@snippet lang=swift :
+         * public var counterInt: Int
+         * }
+         */
+        public static MethodHandle counterInt$set$handle() {
+            return counterInt.HANDLE_SET;
+        }
+        """,
+        """
+        /**
+         * Address for:
+         * {@snippet lang=swift :
+         * public var counterInt: Int
+         * }
+         */
+        public static MemorySegment counterInt$set$address() {
+            return counterInt.ADDR_SET;
+        }
+        """,
+        """
+        /**
+         * Downcall to Swift:
+         * {@snippet lang=swift :
+         * public var counterInt: Int
+         * }
+         */
+        public static long getCounterInt(java.lang.foreign.MemorySegment self$) {
+            var mh$ = counterInt.HANDLE_GET;
+            try {
+                if (TRACE_DOWNCALLS) {
+                    traceDowncall(self$);
+                }
+                return (long) mh$.invokeExact(self$);
+            } catch (Throwable ex$) {
+                throw new AssertionError("should not reach here", ex$);
+            }
+        }
+        """,
+        """
+        /**
+         * Downcall to Swift:
+         * {@snippet lang=swift :
+         * public var counterInt: Int
+         * }
+         */
+        public long getCounterInt() {
+          return (long) getCounterInt($memorySegment());
+        }
+        """,
+        """
+        /**
+         * Downcall to Swift:
+         * {@snippet lang=swift :
+         * public var counterInt: Int
+         * }
+         */
+        public static void setCounterInt(long newValue, java.lang.foreign.MemorySegment self$) {
+          var mh$ = counterInt.HANDLE_SET;
+          try {
+            if (TRACE_DOWNCALLS) {
+               traceDowncall(newValue, self$);
+            }
+             mh$.invokeExact(newValue, self$);
+          } catch (Throwable ex$) {
+            throw new AssertionError("should not reach here", ex$);
+          }
+        }
+        """,
+        """
+        /**
+         * Downcall to Swift:
+         * {@snippet lang=swift :
+         * public var counterInt: Int
+         * }
+         */
+        public void setCounterInt(long newValue) {
+           setCounterInt(newValue, $memorySegment());
+        }
+        """
+      ]
     )
   }
 }
