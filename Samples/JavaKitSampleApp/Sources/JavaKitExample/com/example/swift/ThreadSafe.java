c769b1a4cc57468b70efa14796c817efcfb11dd1
Jack Rosen <jrosen081@gmail.com>
2024-11-25 21:30:58 -0500

    Add Support for Threadsafe and NotThreadSafe annotations (#188)

---
+++ Samples/JavaKitSampleApp/Sources/JavaKitExample/com/example/swift/ThreadSafe.java
@@ -0,0 +1,22 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2024 Apple Inc. and the Swift.org project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+package com.example.swift;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ThreadSafe {
+}
