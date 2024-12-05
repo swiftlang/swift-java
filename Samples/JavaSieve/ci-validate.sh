d65fa596108a2d4d6e5f85ebad6cce75b7c57fe4
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2024-12-05 17:42:08 +0900

    validate JavaSieve in CI

--- Samples/JavaSieve/ci-validate.sh
+++ Samples/JavaSieve/ci-validate.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Not validated in CI..."
+echo "Prepare the dependency..."
+git clone https://github.com/gazman-sdk/quadratic-sieve-Java
+cd quadratic-sieve-Java
+sh ./gradlew jar
+cd ..
+
+echo "Run the sample..."
+swift run JavaSieve
