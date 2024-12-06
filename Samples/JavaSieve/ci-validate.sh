4cc057fb112cf16420f8f2aebf3bd94e22706313
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2024-12-06 11:52:18 +0900

    give up on JavaSieve checking in CI

--- Samples/JavaSieve/ci-validate.sh
+++ Samples/JavaSieve/ci-validate.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-echo "Prepare the dependency..."
-git clone https://github.com/gazman-sdk/quadratic-sieve-Java
-cd quadratic-sieve-Java || exit
-# we use the root gradlew since this project uses ancient gradle that won't support our recent JDK in CI
-sh ../../gradlew jar
-cd .. || exit
-
-echo "Run the sample..."
-swift run JavaSieve
+echo "This sample is not verified in CI because old Gradle in the project making it hard to use its ./gradlew"
