0502ac1cb4106683c275d1c58ab888dd564be165
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2024-12-06 11:35:26 +0900

    fix JavaSieve ci-validate because it uses too old gradle

--- Samples/JavaSieve/ci-validate.sh
+++ Samples/JavaSieve/ci-validate.sh
@@ -3,7 +3,8 @@
 echo "Prepare the dependency..."
 git clone https://github.com/gazman-sdk/quadratic-sieve-Java
 cd quadratic-sieve-Java || exit
-sh ./gradlew jar
+# we use the root gradlew since this project uses ancient gradle that won't support our recent JDK in CI
+sh ../../gradlew jar
 cd .. || exit
 
 echo "Run the sample..."
