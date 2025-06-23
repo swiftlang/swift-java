8461aa053f4854e7ecde87750e9624299193204b
Konrad 'ktoso' Malawski <ktoso@apple.com>
2025-06-23 12:16:54 +0900

    enable complete validation of SwiftAndJavaJarSampleLib on linux

--- Samples/SwiftAndJavaJarSampleLib/ci-validate.sh
+++ Samples/SwiftAndJavaJarSampleLib/ci-validate.sh
@@ -8,16 +8,19 @@ set -x
 # check if we can compile a plain Example file that uses the generated Java bindings that should be in the generated jar
 javac -cp bin/default/build/libs/*jar Example.java
 
-
 if [ "$(uname -s)" = 'Linux' ]
 then
-  exit 1 # not implemented yet
+  SWIFT_LIB_PATHS=$HOME/.local/share/swiftly/toolchains/6.2-snapshot-2025-06-17/usr/lib/swift/linux/
+  SWIFT_LIB_PATHS="${SWIFT_LIB_PATHS}:$(find . | grep libMySwiftLibrary.so$ | sort | head -n1 | xargs dirname)"
 elif [ "$(uname -s)" = 'Darwin' ]
 then
-  # Can we run the example?
   # - find libswiftCore.dylib
-  SWIFT_DYLIB_PATHS=$(find "$(swiftly use --print-location)" | grep dylib$ | grep libswiftCore | grep macos | xargs dirname)
+  SWIFT_LIB_PATHS=$(find "$(swiftly use --print-location)" | grep dylib$ | grep libswiftCore | grep macos | xargs dirname)
   # - find our library dylib
-  SWIFT_DYLIB_PATHS="${SWIFT_DYLIB_PATHS}:$(find . | grep libMySwiftLibrary.dylib$ | sort | head -n1 | xargs dirname)"
-  java -Djava.library.path="${SWIFT_DYLIB_PATHS}" -cp ".:bin/default/build/libs/*jar:../../SwiftKit/build/libs/*jar" Example
+  SWIFT_LIB_PATHS="${SWIFT_LIB_PATHS}:$(find . | grep libMySwiftLibrary.dylib$ | sort | head -n1 | xargs dirname)"
 fi
+
+# Can we run the example?
+java --enable-native-access=ALL-UNNAMED \
+     -Djava.library.path="${SWIFT_LIB_PATHS}" -cp ".:bin/default/build/libs/*:../../SwiftKit/build/libs/*" \
+     Example
