59aef187691d7bfe3eff48c0f842a3ec90debd1a
Konrad 'ktoso' Malawski <ktoso@apple.com>
2025-06-24 13:37:42 +0900

    fix: seems these workaround decls not necessary on 6.1 linux anymore?

--- Samples/SwiftAndJavaJarSampleLib/Sources/MySwiftLibrary/MySwiftLibrary.swift
+++ Samples/SwiftAndJavaJarSampleLib/Sources/MySwiftLibrary/MySwiftLibrary.swift
@@ -38,51 +38,6 @@ public func globalTakeIntInt(i: Int, j: Int) {
 public func globalCallMeRunnable(run: () -> ()) {
   run()
 }
-//
-//public class MySwiftClass {
-//
-//  public var len: Int
-//  public var cap: Int
-//
-//  public init(len: Int, cap: Int) {
-//    self.len = len
-//    self.cap = cap
-//
-//    p("\(MySwiftClass.self).len = \(self.len)")
-//    p("\(MySwiftClass.self).cap = \(self.cap)")
-//    let addr = unsafeBitCast(self, to: UInt64.self)
-//    p("initializer done, self = 0x\(String(addr, radix: 16, uppercase: true))")
-//  }
-//
-//  deinit {
-//    let addr = unsafeBitCast(self, to: UInt64.self)
-//    p("Deinit, self = 0x\(String(addr, radix: 16, uppercase: true))")
-//  }
-//
-//  public var counter: Int32 = 0
-//
-//  public func voidMethod() {
-//    p("")
-//  }
-//
-//  public func takeIntMethod(i: Int) {
-//    p("i:\(i)")
-//  }
-//
-//  public func echoIntMethod(i: Int) -> Int {
-//    p("i:\(i)")
-//    return i
-//  }
-//
-//  public func makeIntMethod() -> Int {
-//    p("make int -> 12")
-//    return 12
-//  }
-//
-//  public func makeRandomIntMethod() -> Int {
-//    return Int.random(in: 1..<256)
-//  }
-//}
 
 // ==== Internal helpers
 
@@ -91,11 +46,3 @@ func p(_ msg: String, file: String = #fileID, line: UInt = #line, function: Stri
   fflush(stdout)
 }
 
-#if os(Linux)
-// FIXME: why do we need this workaround?
-@_silgen_name("_objc_autoreleaseReturnValue")
-public func _objc_autoreleaseReturnValue(a: Any) {}
-
-@_silgen_name("objc_autoreleaseReturnValue")
-public func objc_autoreleaseReturnValue(a: Any) {}
-#endif
