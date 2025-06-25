e66e490f173e4cdfe7fda45147dccf85c451ddcd
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2025-06-25 11:14:42 +0900

    CI: Correct validation & bring back pointer "max" bounds workaround (#284)

--- .github/scripts/validate_sample.sh
+++ .github/scripts/validate_sample.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -x
+
 # shellcheck disable=SC2034
 declare -r GREEN='\033[0;32m'
 declare -r BOLD='\033[1m'
@@ -14,14 +17,11 @@ echo "========================================================================"
 printf "Validate sample '${BOLD}%s${RESET}' using: " "$sampleDir"
 cd "$sampleDir" || exit
 if [[ $(find . -name ${CI_VALIDATE_SCRIPT} -maxdepth 1) ]]; then
-  echo -e "Custom ${BOLD}${CI_VALIDATE_SCRIPT}${RESET} script..."
-  ./${CI_VALIDATE_SCRIPT} || exit
-elif [[ $(find . -name 'build.gradle*' -maxdepth 1) ]]; then
-  echo -e "${BOLD}Gradle${RESET} build..."
-  ./gradlew build || ./gradlew build --info # re-run to get better failure output
+  echo -e "Run ${BOLD}${CI_VALIDATE_SCRIPT}${RESET} script..."
+  ./${CI_VALIDATE_SCRIPT}
 else
-  echo -e "${BOLD}SwiftPM${RESET} build..."
-  swift build || exit
+  echo -e "${BOLD}Missing ${CI_VALIDATE_SCRIPT} file!${RESET}"
+  exit
 fi
 
 echo -e "Validated sample '${BOLD}${sampleDir}${RESET}': ${BOLD}passed${RESET}."
