fdba230556000deff5950fcc6a37aa9347ab15b6
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-09 02:06:53 -0400

    Bump minimum Swift version to 6.1

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import CompilerPluginSupport
 import PackageDescription
 
