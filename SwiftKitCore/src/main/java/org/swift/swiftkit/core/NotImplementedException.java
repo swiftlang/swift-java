1c170d02f7d08af13fe9ea9a5c494619e17f58d6
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2025-07-13 17:39:47 +0900

    SwiftKit: prepare Unsigned Java types for interop with UInt etc

---
+++ SwiftKitCore/src/main/java/org/swift/swiftkit/core/NotImplementedException.java
@@ -0,0 +1,10 @@
+package org.swift.swiftkit.core;
+
+public class NotImplementedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public NotImplementedException(String message) {
+        super(message);
+    }
+}
