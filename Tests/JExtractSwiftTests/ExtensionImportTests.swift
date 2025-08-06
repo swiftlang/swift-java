8d41e6e263b568a9a3adacee0077ac1dfc03762a
Rintaro Ishizaki <fs.output@gmail.com>
2025-08-05 17:09:16 -0700

    [JExtract] Fix importing extensions (#351)

---
+++ Tests/JExtractSwiftTests/ExtensionImportTests.swift
@@ -0,0 +1,48 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift.org project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import JExtractSwiftLib
+import Testing
+
+final class ExtensionImportTests {
+  let interfaceFile =
+    """
+    extension MyStruct {
+      public func methodInExtension() {}
+    }
+
+    public struct MyStruct {}
+    """
+
+  @Test("Import extensions: Swift thunks")
+  func data_swiftThunk() throws {
+    try assertOutput(
+      input: interfaceFile, .ffm, .swift,
+      expectedChunks: [
+        """
+        @_cdecl("swiftjava_getType_SwiftModule_MyStruct")
+        public func swiftjava_getType_SwiftModule_MyStruct() -> UnsafeMutableRawPointer /* Any.Type */ {
+          return unsafeBitCast(MyStruct.self, to: UnsafeMutableRawPointer.self)
+        }
+        """,
+        """
+        @_cdecl("swiftjava_SwiftModule_MyStruct_methodInExtension")
+        public func swiftjava_SwiftModule_MyStruct_methodInExtension(_ self: UnsafeRawPointer) {
+          self.assumingMemoryBound(to: MyStruct.self).pointee.methodInExtension()
+        }
+        """
+      ]
+    )
+  }
+}
