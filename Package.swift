bb44d4cc7f83968952eafe628f634babdb271a8b
Lars <134181853+bo2themax@users.noreply.github.com>
2025-08-29 17:59:18 +0200

    [Test] Add a SimpleJavaProject to JavaRepositoryTests

--- Package.swift
+++ Package.swift
@@ -543,9 +543,10 @@ let package = Package(
     .testTarget(
       name: "SwiftJavaToolTests",
       dependencies: [
-        "SwiftJava",
         "SwiftJavaTool",
-        "JavaNet"
+      ],
+      exclude: [
+        "SimpleJavaProject",
       ],
       swiftSettings: [
         .swiftLanguageMode(.v5),
---
+++ Tests/SwiftJavaToolTests/SimpleJavaProject/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class HelloWorld {
+    public static String sayHello() {
+        return "Hello, world!";
+    }
+}
