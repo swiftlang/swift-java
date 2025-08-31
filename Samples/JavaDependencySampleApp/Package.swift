45c4fdb5ff7b8da55623f597c71fddcf33f1bee2
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-08-30 22:18:23 -0400

    Remove unnecessary import of CSwiftJavaJNI and need for JAVA_HOME include path

--- Samples/JavaDependencySampleApp/Package.swift
+++ Samples/JavaDependencySampleApp/Package.swift
@@ -4,42 +4,6 @@
 import CompilerPluginSupport
 import PackageDescription
 
-import class Foundation.FileManager
-import class Foundation.ProcessInfo
-
-// Note: the JAVA_HOME environment variable must be set to point to where
-// Java is installed, e.g.,
-//   Library/Java/JavaVirtualMachines/openjdk-21.jdk/Contents/Home.
-func findJavaHome() -> String {
-  if let home = ProcessInfo.processInfo.environment["JAVA_HOME"] {
-    return home
-  }
-
-  // This is a workaround for envs (some IDEs) which have trouble with
-  // picking up env variables during the build process
-  let path = "\(FileManager.default.homeDirectoryForCurrentUser.path()).java_home"
-  if let home = try? String(contentsOfFile: path, encoding: .utf8) {
-    if let lastChar = home.last, lastChar.isNewline {
-      return String(home.dropLast())
-    }
-
-    return home
-  }
-
-  fatalError("Please set the JAVA_HOME environment variable to point to where Java is installed.")
-}
-let javaHome = findJavaHome()
-
-let javaIncludePath = "\(javaHome)/include"
-#if os(Linux)
-  let javaPlatformIncludePath = "\(javaIncludePath)/linux"
-#elseif os(macOS)
-  let javaPlatformIncludePath = "\(javaIncludePath)/darwin"
-#else
-  // TODO: Handle windows as well
-  #error("Currently only macOS and Linux platforms are supported, this may change in the future.")
-#endif
-
 let package = Package(
   name: "JavaDependencySampleApp",
   platforms: [
@@ -65,13 +29,11 @@ let package = Package(
       name: "JavaDependencySample",
       dependencies: [
         .product(name: "SwiftJava", package: "swift-java"),
-        .product(name: "CSwiftJavaJNI", package: "swift-java"),
         .product(name: "JavaUtilFunction", package: "swift-java"),
         "JavaCommonsCSV"
       ],
       exclude: ["swift-java.config"],
       swiftSettings: [
-        .unsafeFlags(["-I\(javaIncludePath)", "-I\(javaPlatformIncludePath)"]),
         .swiftLanguageMode(.v5),
       ],
       plugins: [
@@ -90,7 +52,6 @@ let package = Package(
       ],
       exclude: ["swift-java.config"],
       swiftSettings: [
-        .unsafeFlags(["-I\(javaIncludePath)", "-I\(javaPlatformIncludePath)"]),
         .swiftLanguageMode(.v5),
       ],
       plugins: [
--- Samples/SwiftJavaExtractJNISampleApp/Package.swift
+++ Samples/SwiftJavaExtractJNISampleApp/Package.swift
@@ -4,42 +4,6 @@
 import CompilerPluginSupport
 import PackageDescription
 
-import class Foundation.FileManager
-import class Foundation.ProcessInfo
-
-// Note: the JAVA_HOME environment variable must be set to point to where
-// Java is installed, e.g.,
-//   Library/Java/JavaVirtualMachines/openjdk-21.jdk/Contents/Home.
-func findJavaHome() -> String {
-  if let home = ProcessInfo.processInfo.environment["JAVA_HOME"] {
-    return home
-  }
-
-  // This is a workaround for envs (some IDEs) which have trouble with
-  // picking up env variables during the build process
-  let path = "\(FileManager.default.homeDirectoryForCurrentUser.path()).java_home"
-  if let home = try? String(contentsOfFile: path, encoding: .utf8) {
-    if let lastChar = home.last, lastChar.isNewline {
-      return String(home.dropLast())
-    }
-
-    return home
-  }
-
-  fatalError("Please set the JAVA_HOME environment variable to point to where Java is installed.")
-}
-let javaHome = findJavaHome()
-
-let javaIncludePath = "\(javaHome)/include"
-#if os(Linux)
-  let javaPlatformIncludePath = "\(javaIncludePath)/linux"
-#elseif os(macOS)
-  let javaPlatformIncludePath = "\(javaIncludePath)/darwin"
-#else
-  // TODO: Handle windows as well
-  #error("Currently only macOS and Linux platforms are supported, this may change in the future.")
-#endif
-
 let package = Package(
   name: "JExtractJNISampleApp",
   platforms: [
@@ -61,7 +25,6 @@ let package = Package(
       name: "MySwiftLibrary",
       dependencies: [
         .product(name: "SwiftJava", package: "swift-java"),
-        .product(name: "CSwiftJavaJNI", package: "swift-java"),
         .product(name: "SwiftKitSwift", package: "swift-java"),
       ],
       exclude: [
@@ -69,7 +32,6 @@ let package = Package(
       ],
       swiftSettings: [
         .swiftLanguageMode(.v5),
-        .unsafeFlags(["-I\(javaIncludePath)", "-I\(javaPlatformIncludePath)"]),
       ],
       plugins: [
         .plugin(name: "JExtractSwiftPlugin", package: "swift-java")
