499ea036df1edb4860c59931bd4f50c952d5eec5
Konrad Malawski <ktoso@apple.com>
2025-10-24 21:15:37 +0900

    add tests to verify we generated the expected types

---
+++ Samples/SwiftJavaExtractFFMSampleApp/src/test/java/com/example/swift/MultipleTypesFromSingleFileTest.java
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift.org project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+package com.example.swift;
+
+import org.junit.jupiter.api.Test;
+import org.swift.swiftkit.core.*;
+import org.swift.swiftkit.ffm.*;
+
+import java.lang.foreign.Arena;
+import java.util.Optional;
+import java.util.OptionalInt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MultipleTypesFromSingleFileTest {
+    
+    @Test
+    void bothTypesMustHaveBeenGenerated() {
+        try (var arena = AllocatingSwiftArena.ofConfined()) {
+            PublicTypeOne.init(arena);
+            PublicTypeTwo.init(arena);
+        }
+    }
+}
