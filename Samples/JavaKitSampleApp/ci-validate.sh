2d932e6b02f07fd9e8a83ba112d2767f0677da37
Konrad Malawski <ktoso@apple.com>
2025-11-11 09:06:26 +0900

    try debugging JNI on CI

--- Samples/JavaKitSampleApp/ci-validate.sh
+++ Samples/JavaKitSampleApp/ci-validate.sh
@@ -5,6 +5,7 @@ set -x
 
 swift build
 "$JAVA_HOME/bin/java" \
+    -verbose:jni \
     -cp .build/plugins/outputs/javakitsampleapp/JavaKitExample/destination/JavaCompilerPlugin/Java \
     -Djava.library.path=.build/debug \
     "com.example.swift.JavaKitSampleMain"
