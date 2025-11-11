31e1d9b449ad38ca84af8de3aa4163b410f43d13
Konrad Malawski <ktoso@apple.com>
2025-11-11 23:55:27 +0900

    shouldn't we use the deferEnv here?

--- Sources/JExtractSwiftLib/JNI/JNISwift2JavaGenerator+NativeTranslation.swift
+++ Sources/JExtractSwiftLib/JNI/JNISwift2JavaGenerator+NativeTranslation.swift
@@ -852,7 +852,7 @@ extension JNISwift2JavaGenerator {
             // Defer might on any thread, so we need to attach environment.
             printer.print("let deferEnvironment = try! JavaVirtualMachine.shared().environment()")
             for globalRef in globalRefs {
-              printer.print("environment.interface.DeleteGlobalRef(deferEnvironment, \(globalRef))")
+              printer.print("deferEnvironment.interface.DeleteGlobalRef(deferEnvironment, \(globalRef))")
             }
           }
           if isThrowing {
