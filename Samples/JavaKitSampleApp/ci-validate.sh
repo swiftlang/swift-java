636387abcd70c6a64ec592e070f09716e944e997
Konrad Malawski <ktoso@apple.com>
2025-11-11 23:55:23 +0900

    remove verbose output options

--- Samples/JavaKitSampleApp/ci-validate.sh
+++ Samples/JavaKitSampleApp/ci-validate.sh
@@ -5,7 +5,6 @@ set -x
 
 swift build
 "$JAVA_HOME/bin/java" \
-    -verbose:jni \
     -cp .build/plugins/outputs/javakitsampleapp/JavaKitExample/destination/JavaCompilerPlugin/Java \
     -Djava.library.path=.build/debug \
     "com.example.swift.JavaKitSampleMain"
