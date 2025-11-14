15df65ba24e80ff9fcf110a6bec79e6c8a8a4c53
Konrad Malawski <ktoso@apple.com>
2025-11-14 12:22:19 +0900

    Allow // comments in swift-java.config "JSON with comments"

---
+++ Tests/SwiftJavaConfigurationSharedTests/SwiftJavaConfigurationTests.swift
@@ -0,0 +1,33 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2024-2025 Apple Inc. and the Swift.org project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import SwiftJavaConfigurationShared
+import Testing
+
+@Suite
+struct SwiftJavaConfigurationTests {
+
+  @Test
+  func parseJSONWithComments() throws {
+    let config = try readConfiguration(string: 
+      """
+      // some comments
+      {
+        // anywhere is ok
+        "classpath": ""
+      }
+      """, configPath: nil)
+  }
+}
+
