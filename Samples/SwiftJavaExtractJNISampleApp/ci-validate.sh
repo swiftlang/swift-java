9001eca3f5852c48a19b0046467988c930070fab
Mads Odgaard <mads.odgaard@frameo.com>
2025-11-21 09:02:54 +0100

    disable sandbox in ci-validate

--- Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
+++ Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-swift build --disable-experimental-prebuilts  # FIXME: until prebuilt swift-syntax isn't broken on 6.2 anymore: https://github.com/swiftlang/swift-java/issues/418
+swift build --disable-experimental-prebuilts --disable-sandbox  # FIXME: until prebuilt swift-syntax isn't broken on 6.2 anymore: https://github.com/swiftlang/swift-java/issues/418
 
 ./gradlew run
 ./gradlew test
