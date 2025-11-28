fc5f9ecbfb80c1ca6231ba690e651bece1a7ab9e
Konrad `ktoso` Malawski <konrad.malawski@project13.pl>
2025-11-28 18:03:42 +0900

    Add command to fetch Gradle wrapper in ci-validate.sh
    Added command to fetch Gradle wrapper for debugging CI issues.

--- Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
+++ Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
@@ -3,6 +3,8 @@
 set -x
 set -e
 
+./gradlew -h  # force fetching gradle wrapper (debugging CI gradlew issues)
+
 swift build --disable-experimental-prebuilts --disable-sandbox --verbose  # FIXME: until prebuilt swift-syntax isn't broken on 6.2 anymore: https://github.com/swiftlang/swift-java/issues/418
 
 ./gradlew run
