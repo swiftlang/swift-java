e82e1bef1c3cbd80d038e08ba74052e02c6b49e6
Konrad `ktoso` Malawski <konrad.malawski@project13.pl>
2025-12-01 15:00:31 +0900

    ci: Modify ci-validate.sh for gradle build testing
    Updated CI validation script to include gradle build command.

--- Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
+++ Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
@@ -3,7 +3,8 @@
 set -x
 set -e
 
-./gradlew -h  # force fetching gradle wrapper (debugging CI gradlew issues)
+// FIXME: debugging CI issue; test gradle command by itself
+/Users/runner/actions-runner/_work/swift-java/swift-java/gradlew :SwiftKitCore:build -p /Users/runner/actions-runner/_work/swift-java/swift-java/ --configure-on-demand --no-daemon
 
 swift build --disable-experimental-prebuilts --disable-sandbox --verbose  # FIXME: until prebuilt swift-syntax isn't broken on 6.2 anymore: https://github.com/swiftlang/swift-java/issues/418
 
