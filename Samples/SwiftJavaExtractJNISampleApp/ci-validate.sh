03f27a6ef3de5aa45015da3d68cf5c8180809991
Konrad `ktoso` Malawski <konrad.malawski@project13.pl>
2025-12-01 16:16:21 +0900

    ci: try to copy existing gradle home to avoid gradlew fetch issue on ci
    Updated CI script to handle gradle wrapper download issues and set custom gradle user home.

--- Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
+++ Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
@@ -3,12 +3,16 @@
 set -x
 set -e
 
-# FIXME: debugging CI issue; test gradle command by itself
-pwd
-ls /Users/runner/actions-runner/_work/swift-java/
-ls /Users/runner/actions-runner/_work/swift-java/swift-java
+# GitHub Actions workaround; We seem to be hitting gradle wrapper download issues, and this attempts to workaround them
+export GRADLE_USER_HOME="$(pwd)/.build/plugins/outputs/swiftjavaextractjnisampleapp/MySwiftLibrary/destination/JExtractSwiftPlugin/gradle-user-home"
+if [ -d "$HOME/.gradle" ] ; then
+  echo "COPY $HOME/.gradle to $GRADLE_USER_HOME"
+  mkdir -p "$GRADLE_USER_HOME"
+  cp -r "$HOME/.gradle/*" "$GRADLE_USER_HOME/" || true
+fi
 
-GRADLE_USER_HOME=/Users/runner/actions-runner/_work/swift-java/swift-java/Samples/SwiftJavaExtractJNISampleApp/.build/plugins/outputs/swiftjavaextractjnisampleapp/MySwiftLibrary/destination/JExtractSwiftPlugin/gradle-user-home ./gradlew :SwiftKitCore:build -p /Users/runner/actions-runner/_work/swift-java/swift-java/ --configure-on-demand --no-daemon
+# Verify the custom gradle home directory works  # TODO: remove this once verified
+./gradlew :SwiftKitCore:build --gradle-user-home $GRADLE_USER_HOME -p "$(pwd)/../../" --configure-on-demand --no-daemon
 
 swift build --disable-experimental-prebuilts --disable-sandbox --verbose  # FIXME: until prebuilt swift-syntax isn't broken on 6.2 anymore: https://github.com/swiftlang/swift-java/issues/418
 
