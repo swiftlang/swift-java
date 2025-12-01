25899854ada74f7ea52465901e375547aff6dad9
Konrad `ktoso` Malawski <konrad.malawski@project13.pl>
2025-12-01 15:21:14 +0900

    Fix comment syntax in ci-validate.sh

--- Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
+++ Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-// FIXME: debugging CI issue; test gradle command by itself
+# FIXME: debugging CI issue; test gradle command by itself
 /Users/runner/actions-runner/_work/swift-java/swift-java/gradlew :SwiftKitCore:build -p /Users/runner/actions-runner/_work/swift-java/swift-java/ --configure-on-demand --no-daemon
 
 swift build --disable-experimental-prebuilts --disable-sandbox --verbose  # FIXME: until prebuilt swift-syntax isn't broken on 6.2 anymore: https://github.com/swiftlang/swift-java/issues/418
