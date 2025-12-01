23b9cf5027f2e80691590541847589f8017423c5
Konrad `ktoso` Malawski <konrad.malawski@project13.pl>
2025-12-01 15:29:06 +0900

    ci: more CI debugging specific gradlew command
    Updated gradle command to set GRADLE_USER_HOME for the build process.

--- Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
+++ Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 # FIXME: debugging CI issue; test gradle command by itself
-/Users/runner/actions-runner/_work/swift-java/swift-java/gradlew :SwiftKitCore:build -p /Users/runner/actions-runner/_work/swift-java/swift-java/ --configure-on-demand --no-daemon
+GRADLE_USER_HOME=/Users/runner/actions-runner/_work/swift-java/swift-java/Samples/SwiftJavaExtractJNISampleApp/.build/plugins/outputs/swiftjavaextractjnisampleapp/MySwiftLibrary/destination/JExtractSwiftPlugin/gradle-user-home ./gradlew :SwiftKitCore:build -p /Users/runner/actions-runner/_work/swift-java/swift-java/ --configure-on-demand --no-daemon
 
 swift build --disable-experimental-prebuilts --disable-sandbox --verbose  # FIXME: until prebuilt swift-syntax isn't broken on 6.2 anymore: https://github.com/swiftlang/swift-java/issues/418
 
