6e0c6c3ba16def7a68a85c5b970f14a616a09cb4
Konrad `ktoso` Malawski <konrad.malawski@project13.pl>
2025-12-01 15:31:28 +0900

    ci: ci debugging paths
    Added debugging commands to list directory contents for CI troubleshooting.

--- Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
+++ Samples/SwiftJavaExtractJNISampleApp/ci-validate.sh
@@ -4,6 +4,9 @@ set -x
 set -e
 
 # FIXME: debugging CI issue; test gradle command by itself
+ls /Users/runner/actions-runner/_work/swift-java/
+ls /Users/runner/actions-runner/_work/swift-java/swift-java
+
 GRADLE_USER_HOME=/Users/runner/actions-runner/_work/swift-java/swift-java/Samples/SwiftJavaExtractJNISampleApp/.build/plugins/outputs/swiftjavaextractjnisampleapp/MySwiftLibrary/destination/JExtractSwiftPlugin/gradle-user-home ./gradlew :SwiftKitCore:build -p /Users/runner/actions-runner/_work/swift-java/swift-java/ --configure-on-demand --no-daemon
 
 swift build --disable-experimental-prebuilts --disable-sandbox --verbose  # FIXME: until prebuilt swift-syntax isn't broken on 6.2 anymore: https://github.com/swiftlang/swift-java/issues/418
