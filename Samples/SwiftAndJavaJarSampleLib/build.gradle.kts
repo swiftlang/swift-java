3cb749ff1466d9398699db8d4710cd14cf107709
Timofey Solonin <abdulowork@gmail.com>
2026-01-20 13:18:12 +0100

    Add missing licenses

--- Samples/SwiftAndJavaJarSampleLib/build.gradle.kts
+++ Samples/SwiftAndJavaJarSampleLib/build.gradle.kts
@@ -11,6 +11,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
+
 import utilities.registerJextractTask
 
 plugins {
--- Samples/SwiftJavaExtractJNISampleApp/build.gradle.kts
+++ Samples/SwiftJavaExtractJNISampleApp/build.gradle.kts
@@ -11,6 +11,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
+
 import utilities.javaLibraryPaths
 import utilities.registerJextractTask
 
