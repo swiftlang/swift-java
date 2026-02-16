86ce9ac05f186682703914ac6b219b596b2cb0cc
Iceman <okamura@qoncept.co.jp>
2026-02-16 17:51:19 +0900

    Add test case for swift side generated code

--- Tests/JExtractSwiftTests/JNI/JNIGenericTypeTests.swift
+++ Tests/JExtractSwiftTests/JNI/JNIGenericTypeTests.swift
@@ -61,10 +61,77 @@ struct JNIGenericTypeTests {
         public long $typeMetadataAddress() {
           return this.selfTypePointer;
         }
-          }
-          return MyID.$typeMetadataAddressDowncall(this.t0MetaPointer);
-        }
+        """,
+      ]
+    )
+  }
+
+  @Test
+  func generateSwiftThunk() throws {
+    try assertOutput(
+      input: genericFile,
+      .jni,
+      .swift,
+      detectChunkByInitialLines: 2,
+      expectedChunks: [
         """
+        protocol _SwiftModule_MyID_opener {
+          static func _get_description(selfBits$: Int) -> String
+          static func _toString(selfBits$: Int) -> String
+          static func _toDebugString(selfBits$: Int) -> String
+        }
+        """,
+        """
+        extension MyID: _SwiftModule_MyID_opener {
+          static func _get_description(selfBits$: Int) -> String {
+            let self$ = UnsafeMutablePointer<Self>(bitPattern: selfBits$)
+            guard let self$ else {
+              fatalError("self memory address was null in call to \\(#function)!")
+            }
+            return self$.pointee.description
+          }
+          static func _toString(selfBits$: Int) -> String {
+            let self$ = UnsafeMutablePointer<Self>(bitPattern: selfBits$)
+            guard let self$ else {
+              fatalError("self memory address was null in call to \\(#function)!")
+            }
+            return String(describing: self$.pointee)
+          }
+          static func _toDebugString(selfBits$: Int) -> String {
+            let self$ = UnsafeMutablePointer<Self>(bitPattern: selfBits$)
+            guard let self$ else {
+              fatalError("self memory address was null in call to \\(#function)!")
+            }
+            return String(reflecting: self$.pointee)
+          }
+        } 
+        """,
+        """
+        @_cdecl("Java_com_example_swift_MyID__00024getDescription__J")
+        public func Java_com_example_swift_MyID__00024getDescription__J(environment: UnsafeMutablePointer<JNIEnv?>!, thisClass: jclass, self: jlong, selfType: jlong) -> jstring? {
+          assert(self != 0, "self memory address was null")
+          let selfBits$ = Int(Int64(fromJNI: self, in: environment))
+          guard let selfTypeMetadataPointer$ = UnsafeRawPointer(bitPattern: Int(Int64(fromJNI: selfType, in: environment))) else {
+            fatalError("selfType memory address was null")
+          }
+          let erasedSelfType$: Any.Type = unsafeBitCast(selfTypeMetadataPointer$, to: Any.Type.self)
+          let openerType = erasedSelfType$ as! (any _SwiftModule_MyID_opener.Type)
+          return openerType._get_description(selfBits$: selfBits$).getJNIValue(in: environment)
+        }
+        """,
+        """
+        @_cdecl("Java_com_example_swift_MyID__00024toString__J")
+        public func Java_com_example_swift_MyID__00024toString__J(environment: UnsafeMutablePointer<JNIEnv?>!, thisClass: jclass, self: jlong, selfType: jlong) -> jstring? {
+          assert(self != 0, "self memory address was null")
+          let selfBits$ = Int(Int64(fromJNI: self, in: environment))
+          guard let selfTypeMetadataPointer$ = UnsafeRawPointer(bitPattern: Int(Int64(fromJNI: selfType, in: environment))) else {
+            fatalError("selfType memory address was null")
+          }
+          let erasedSelfType$: Any.Type = unsafeBitCast(selfTypeMetadataPointer$, to: Any.Type.self)
+          let openerType = erasedSelfType$ as! (any _SwiftModule_MyID_opener.Type)
+          return openerType._toString(selfBits$: selfBits$).getJNIValue(in: environment)
+        }
+        """,
       ]
     )
   }
