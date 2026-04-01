bd8b411c612fcb443467d5e29abe00ba5d92d625
Iceman <okamura@qoncept.co.jp>
2026-04-01 17:03:41 +0900

    Revert package.swift updates

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let swiftJavaJNICoreDep: Package.Dependency
 if let localPath = Context.environment["SWIFT_JAVA_JNI_CORE_PATH"] {
   swiftJavaJNICoreDep = .package(path: localPath)
 } else {
-  swiftJavaJNICoreDep = .package(url: "https://github.com/sidepelican/swift-java-jni-core", branch: "fixed_witdh_integer")
+  swiftJavaJNICoreDep = .package(url: "https://github.com/swiftlang/swift-java-jni-core", branch: "main")
 }
 
 let package = Package(
