09cedb6def7512636def69f7ca53ce3fa932b187
Iceman <okamura@qoncept.co.jp>
2026-05-08 16:25:34 +0900

    slim test code

--- Tests/JExtractSwiftTests/JNI/JNICollectionBoxableTests.swift
+++ Tests/JExtractSwiftTests/JNI/JNICollectionBoxableTests.swift
@@ -17,8 +17,8 @@ import Testing
 
 @Suite
 struct JNICollectionBoxableTests {
-  @Test("JNI generates JavaValue and JavaBoxable for dictionary element types")
-  func dictionaryCustomValueGeneratesJavaBoxingConformance() throws {
+  @Test("JNI generates JavaBoxable for dictionary element types")
+  func generatesJavaBoxableConformance() throws {
     try assertOutput(
       input: """
         public struct ReefFish: Hashable {}
@@ -29,29 +29,15 @@ struct JNICollectionBoxableTests {
       detectChunkByInitialLines: 1,
       expectedChunks: [
         """
-        @_cdecl("Java_com_example_swift_SwiftModule__00024f__J")
-        public func Java_com_example_swift_SwiftModule__00024f__J(environment: UnsafeMutablePointer<JNIEnv?>!, thisClass: jclass, dict: jlong) -> jlong {
-          return SwiftModule.f(dict: [Int: ReefFish](fromJNI: dict, in: environment)).dictionaryGetJNIValue(in: environment)
-        }
-        """,
-        """
         private enum _SwiftJavaBoxing_ReefFish {
           private static let wrapMemoryAddressUnsafeMethod = _JNIMethodIDCache.Method(
             name: "wrapMemoryAddressUnsafe",
             signature: "(J)Lcom/example/swift/ReefFish;",
             isStatic: true
           )
-          ...
-        }
         """,
         """
-        extension ReefFish: JavaValue, JavaBoxable {
-          public typealias JNIType = jobject?
-          public static var jvalueKeyPath: WritableKeyPath<jvalue, JNIType> { \\.l }
-          public static var javaType: JavaType { JavaType(className: "com.example.swift.ReefFish") }
-          public func getJNIValue(in environment: JNIEnvironment) -> JNIType {
-            toJavaObject(in: environment)
-          }
+        extension ReefFish: JavaBoxable {
           public func toJavaObject(in environment: JNIEnvironment) -> jobject? {
             let selfPointer$ = UnsafeMutablePointer<Self>.allocate(capacity: 1)
             selfPointer$.initialize(to: self)
@@ -82,169 +68,19 @@ struct JNICollectionBoxableTests {
             }
             return valuePointer$.pointee
           }
-          ...
-          public static var jniPlaceholderValue: JNIType { nil }
         }
         """,
-      ],
-      notExpectedChunks: ["private enum _SwiftJavaCollectionJavaBoxingCache"]
-    )
-  }
-
-  @Test("JNI generates JavaValue and JavaBoxable for set element types")
-  func setCustomElementGeneratesJavaBoxingConformance() throws {
-    try assertOutput(
-      input: """
-        public struct ReefFish: Hashable {}
-        public func f(set: Set<ReefFish>) -> Set<ReefFish> {}
-        """,
-      .jni,
-      .swift,
-      detectChunkByInitialLines: 1,
-      expectedChunks: [
-        """
-        @_cdecl("Java_com_example_swift_SwiftModule__00024f__J")
-        public func Java_com_example_swift_SwiftModule__00024f__J(environment: UnsafeMutablePointer<JNIEnv?>!, thisClass: jclass, set: jlong) -> jlong {
-          return SwiftModule.f(set: Set<ReefFish>(fromJNI: set, in: environment)).setGetJNIValue(in: environment)
-        }
-        """,
-        """
-        extension ReefFish: JavaValue, JavaBoxable {
-          public typealias JNIType = jobject?
-          public static var jvalueKeyPath: WritableKeyPath<jvalue, JNIType> { \\.l }
-          public static var javaType: JavaType { JavaType(className: "com.example.swift.ReefFish") }
-          public func getJNIValue(in environment: JNIEnvironment) -> JNIType {
-            toJavaObject(in: environment)
-          }
-          public func toJavaObject(in environment: JNIEnvironment) -> jobject? {
-            let selfPointer$ = UnsafeMutablePointer<Self>.allocate(capacity: 1)
-            selfPointer$.initialize(to: self)
-            let selfPointerBits$ = Int64(Int(bitPattern: selfPointer$))
-            var args = [jvalue()]
-            args[0].j = selfPointerBits$.getJNIValue(in: environment)
-            return environment.interface.CallStaticObjectMethodA(
-              environment,
-              _SwiftJavaBoxing_ReefFish.javaClass,
-              _SwiftJavaBoxing_ReefFish.wrapMemoryAddressUnsafe,
-              &args
-            )
-          }
-          ...
-          public static func fromJavaObject(_ obj: jobject?, in environment: JNIEnvironment) -> Self {
-            guard let obj else {
-              fatalError("ReefFish.fromJavaObject received a null Java object")
-            }
-            let selfPointer$ = environment.interface.CallLongMethodA(
-              environment,
-              obj,
-              _JNIMethodIDCache.JNISwiftInstance.memoryAddress,
-              nil
-            )
-            let selfPointerBits$ = Int(Int64(fromJNI: selfPointer$, in: environment))
-            guard let valuePointer$ = UnsafeMutablePointer<Self>(bitPattern: selfPointerBits$) else {
-              fatalError("ReefFish.fromJavaObject received a null Swift memory address")
-            }
-            return valuePointer$.pointee
-          }
-          ...
-          public static func jniNewArray(in environment: JNIEnvironment) -> JNINewArray {
-            { environment, size in environment.interface.NewObjectArray(environment, size, _SwiftJavaBoxing_ReefFish.javaClass, nil) }
-          }
-          ...
-        }
-        """,
-      ],
-      notExpectedChunks: ["private enum _SwiftJavaCollectionJavaBoxingCache"]
-    )
-  }
-
-  @Test("JNI supports optional collection values without generating optional boxing")
-  func dictionaryOptionalValueUsesRuntimeOptionalBoxing() throws {
-    try assertOutput(
-      input: """
-        public struct ReefFish: Hashable {}
-        public func f(dict: [Int: ReefFish?]) -> [Int: ReefFish?] {}
-        """,
-      .jni,
-      .swift,
-      detectChunkByInitialLines: 1,
-      expectedChunks: [
-        """
-        @_cdecl("Java_com_example_swift_SwiftModule__00024f__J")
-        public func Java_com_example_swift_SwiftModule__00024f__J(environment: UnsafeMutablePointer<JNIEnv?>!, thisClass: jclass, dict: jlong) -> jlong {
-          return SwiftModule.f(dict: [Int: ReefFish?](fromJNI: dict, in: environment)).dictionaryGetJNIValue(in: environment)
-        }
-        """,
-        """
-        extension ReefFish: JavaValue, JavaBoxable {
-          ...
-        }
-        """,
-      ],
-      notExpectedChunks: [
-        "extension Optional<ReefFish>: JavaValue, JavaBoxable",
-        "extension ReefFish?: JavaValue, JavaBoxable",
       ]
     )
   }
 
-  @Test("JNI uses runtime support cache for nested dictionary boxing")
-  func nestedDictionaryUsesRuntimeSupportCache() throws {
-    try assertOutput(
-      input: """
-        public struct ReefFish: Hashable {}
-        public func f(dict: [String: [Int: ReefFish]]) -> [String: [Int: ReefFish]] {}
-        """,
-      .jni,
-      .swift,
-      detectChunkByInitialLines: 1,
-      expectedChunks: [
-        """
-        @_cdecl("Java_com_example_swift_SwiftModule__00024f__J")
-        public func Java_com_example_swift_SwiftModule__00024f__J(environment: UnsafeMutablePointer<JNIEnv?>!, thisClass: jclass, dict: jlong) -> jlong {
-          return SwiftModule.f(dict: [String: [Int: ReefFish]](fromJNI: dict, in: environment)).dictionaryGetJNIValue(in: environment)
-        }
-        """,
-      ],
-      notExpectedChunks: [
-        "private enum _SwiftJavaCollectionJavaBoxingCache",
-        "extension [Int: ReefFish]: JavaValue, JavaBoxable",
-      ]
-    )
-  }
-
-  @Test("JNI uses runtime support cache for nested set boxing")
-  func nestedSetUsesRuntimeSupportCache() throws {
-    try assertOutput(
-      input: """
-        public struct ReefFish: Hashable {}
-        public func f(dict: [String: Set<ReefFish>]) -> [String: Set<ReefFish>] {}
-        """,
-      .jni,
-      .swift,
-      detectChunkByInitialLines: 1,
-      expectedChunks: [
-        """
-        @_cdecl("Java_com_example_swift_SwiftModule__00024f__J")
-        public func Java_com_example_swift_SwiftModule__00024f__J(environment: UnsafeMutablePointer<JNIEnv?>!, thisClass: jclass, dict: jlong) -> jlong {
-          return SwiftModule.f(dict: [String: Set<ReefFish>](fromJNI: dict, in: environment)).dictionaryGetJNIValue(in: environment)
-        }
-        """,
-      ],
-      notExpectedChunks: [
-        "private enum _SwiftJavaCollectionJavaBoxingCache",
-        "extension Set<ReefFish>: JavaValue, JavaBoxable",
-      ]
-    )
-  }
 
   @Test("JNI generates JavaBoxable for generic dictionary keys")
-  func genericDictionaryKeyGeneratesJavaBoxingConformance() throws {
+  func generatesJavaBoxableConformanceForGenericType() throws {
     try assertOutput(
       input: """
-        public struct Fish: Hashable {}
         public struct MyID<T>: Hashable {}
-        public func f() -> [MyID<Int>: Fish] {}
+        public func f() -> [MyID<Int>: String] {}
         """,
       .jni,
       .swift,
@@ -257,17 +93,9 @@ struct JNICollectionBoxableTests {
             signature: "(JJ)Lcom/example/swift/MyID;",
             isStatic: true
           )
-          ...
-        }
         """,
         """
-        extension MyID: JavaValue, JavaBoxable {
-          public typealias JNIType = jobject?
-          public static var jvalueKeyPath: WritableKeyPath<jvalue, JNIType> { \\.l }
-          public static var javaType: JavaType { JavaType(className: "com.example.swift.MyID") }
-          public func getJNIValue(in environment: JNIEnvironment) -> JNIType {
-            toJavaObject(in: environment)
-          }
+        extension MyID: JavaBoxable {
           public func toJavaObject(in environment: JNIEnvironment) -> jobject? {
             let selfPointer$ = UnsafeMutablePointer<Self>.allocate(capacity: 1)
             selfPointer$.initialize(to: self)
@@ -301,7 +129,6 @@ struct JNICollectionBoxableTests {
             }
             return valuePointer$.pointee
           }
-          ...
         }
         """,
       ]
