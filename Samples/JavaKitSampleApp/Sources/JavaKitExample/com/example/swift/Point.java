53c156dc14e29b67682125b7f97e80df65679c83
Konrad Malawski <ktoso@apple.com>
2026-07-13 10:42:56 +0900

    wrap-java: support java record types
    Simple addition to support record types using existing infra.
    This adds a new macro but it is just sugar over the existing JavaClass
    macro

---
+++ Samples/JavaKitSampleApp/Sources/JavaKitExample/com/example/swift/Point.java
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2026 Apple Inc. and the Swift.org project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+package com.example.swift;
+
+/**
+ * Java 16+ record used to smoke-test wrap-java's <code>@JavaRecord</code> support.
+ */
+public record Point(int x, int y) {}
