59d9a272339e1dd3ddf524842742bd3f7fd07e64
Konrad `ktoso` Malawski <konrad.malawski@project13.pl>
2026-07-18 10:47:12 +0900

    Apply suggestions from code review

--- Sources/JExtractSwiftLib/JNI/JNISwift2JavaGenerator+JavaBindingsPrinting.swift
+++ Sources/JExtractSwiftLib/JNI/JNISwift2JavaGenerator+JavaBindingsPrinting.swift
@@ -791,8 +791,10 @@ extension JNISwift2JavaGenerator {
       return
     }
 
-    // If it's contain one function type and it's known java functional interface type, we don't need to print the whole class
-    if translated.functionTypes.count == 1 && KnownJavaFunctionalInterface.find(translated.functionTypes[0]) != nil {
+    // If contains one function type and it's known java functional interface type, we don't need to print the whole class
+    if translated.functionTypes.count == 1, 
+       let ty = translated.functionTypes.first, 
+       KnownJavaFunctionalInterface.find(ty) != nil {
       return
     }
 
